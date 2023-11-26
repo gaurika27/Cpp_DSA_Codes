6384167b0684c1453d5b1de33a9c7324cec0e32c
gaurika27 <121048762+gaurika27@users.noreply.github.com>
2023-11-26 14:54:46 +0530

    graphs

--- lec76_Graphs.cpp
+++ lec76_Graphs.cpp
@@ -1,5 +1,12 @@
-#include<iostream.
+#include<iostream>
+#include<unordered_map>
+#include<queue>
+#include<set>
+#include<list>
+
 using namespace std;
+
+
 
 void prepareAdjList(unordered_map<int, list<int> > &adjList, vector<pair<int, int>> &edges){
   for(int i=0; i<edges.size(); i++){
@@ -11,10 +18,37 @@ void prepareAdjList(unordered_map<int, list<int> > &adjList, vector<pair<int, in
   }
 }
 
+void bfs(unordered_map<int, list<int> > &adjList, unordered_map<int, bool> visited, vector<int> &ans, int node){
+  queue<int> q;
+  q.push(node);
+  visited[node]==1;
+
+  while(!q.empty()){
+    int frontNode=q.front();
+    q.pop();
+  }
+
+  //store frontNode into ans
+  ans.push_back(frontNode);
+
+  //traverse all neighbours of frontNode
+  for(auto i:adjList{frontNode}){
+    if(!visited[i]){
+      q.push(i);
+      visited[i]=1;
+    }
+  }
+}
 vector<int> BFS(int vertex, vector<pair<int, int>> edges){
   unordered_map<int, list<int>> adjList;
   vector<int> ans;
   unordered_map<int, bool> visited;
   prepareAdjList(adjList,edges);
   
+  //traverse all the components of a graph
+  for(int i=0;  i<vertex; i++){
+    if(visited[i]){
+      bfs(adjList,visited,ans ,i);
+    }
+  }
 }
