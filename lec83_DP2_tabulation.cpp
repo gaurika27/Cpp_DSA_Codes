ccf3e2981e4eadcc79baffe1301f01f5a660a0a7
gaurika27 <121048762+gaurika27@users.noreply.github.com>
2024-02-01 15:24:05 +0530

    tabulation in dynamic programming

--- lec83_DP2_tabulation.cpp
+++ lec83_DP2_tabulation.cpp
@@ -2,6 +2,22 @@
 //Fibonacci series using DP(tabulation)
 //TC:O(N)
 //SC:O(N)-> more effective
-#include<iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 0;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
