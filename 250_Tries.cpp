e1137d597b409f1d6ce23f42db40f31e159a7eb0
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-06-05 19:11:10 +0530

    tries definition

---
+++ 250_Tries.cpp
@@ -0,0 +1,5 @@
+/*
+Trie is a data structure that is like a tree data structure in its organisation. It consists of nodes that store letters or alphabets of words, which can be added, retrieved, and deleted from it in a very efficient way.
+
+In other words, Trie is an information retrieval data structure, which can beat naive data structures like Hashmap, Tree, etc in the time complexity of its operations.
+*/
