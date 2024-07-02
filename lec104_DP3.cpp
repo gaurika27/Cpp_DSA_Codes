38b2f1455af94961c449e2471522740b09e0a8d1
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-07-02 18:04:33 +0530

    shortcut for recursion in  dp

---
+++ lec104_DP3.cpp
@@ -0,0 +1,7 @@
+/*
+//Shortcut for recursion
+1)Try to represent the problem in terms of index
+2)Do all possible stuff on that index according to the problem statement.
+3.a)Sum of all stuffs->count all ways
+3.b)min/max of all stuffs->find min/max
+*/
