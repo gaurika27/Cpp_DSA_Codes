c4acb7a89c67f7d1c5e1acbd0be071ad43292dba
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2024-08-21 00:20:54 +0530

    done

--- lec251_Heaps_Insertion_Deletion.cpp
+++ lec251_Heaps_Insertion_Deletion.cpp
@@ -22,10 +22,37 @@ class heap{
         }
       }
     }
-    void parent(){
+    void print(){
       for(int i=1; i<=size; i++){
         cout<<arr[i]<<" ";
       }cout<<endl;
+    }
+
+
+    void deletefromHeap(){
+      if(size==0) return;
+
+      //Step1: put last element innto first index
+      arr[1]=arr[size];
+      //Step2: remove the last element
+      size--;
+
+      //tstep3:ake root to its correct position
+      int i=1;
+      while(i<size){
+        int leftIndex=2*i;
+        int rightIndex=2*i+1;
+
+        if(leftIndex<size && arr[i]<arr[leftIndex]){
+          swap(arr[i], arr[leftIndex]);
+          i=leftIndex;
+        }else if(rightIndex<size && arr[i]<arr[rightIndex]){
+          swap(arr[i], arr[rightIndex]);
+          i=rightIndex;
+        }else{
+          return;
+        }
+      }
     }
 };
 
@@ -36,6 +63,8 @@ int main(){
   h.insert(53);
   h.insert(52);
   h.insert(45);
+  h.print();
+  h.deletefromHeap();
   h.print();
   
   return 0;
