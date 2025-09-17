12c36ad8b68947edaa215aa0efc304fe0be5acc6
Gaurika Sharma <121048762+gaurika27@users.noreply.github.com>
2025-09-17 17:08:33 +0530

    Update Lec1_SortingMethods.cpp

--- Sorting/Lec1_SortingMethods.cpp
+++ Sorting/Lec1_SortingMethods.cpp
@@ -1,68 +1,68 @@
 //Sorting in arrays
 //1)Selection Sort:Find the minimum element in the unsorted array and swap it with the element in the beginning of the remaining unsorted array
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//   int n;
-//   cin>>n;
+int main(){
+  int n;
+  cin>>n;
 
-//   int arr[n];
-//   for(int i=0; i<=n; i++){
-//     cin>>arr[i];
-//   }
+  int arr[n];
+  for(int i=0; i<=n; i++){
+    cin>>arr[i];
+  }
 
-//   for(int i=0; i<=n-1; i++){
-//     for(int j=i+1; j<n; j++){
-//       if(arr[j]=arr[i]){
-//         int temp=arr[j];
-//         arr[j]=arr[i];
-//         arr[i]=temp;
-//       }      
-//     }
-//   }
+  for(int i=0; i<=n-1; i++){
+    for(int j=i+1; j<n; j++){
+      if(arr[j]=arr[i]){
+        int temp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=temp;
+      }      
+    }
+  }
 
-//   for(int i=0; i<n; i++){
-//     cout<<arr[i]<<" ";
-//   }cout<<endl;
+  for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }cout<<endl;
 
-//  return 0; 
-//  }
+ return 0; 
+ }
 
 
  //2)Bubble Sort:Repeatedly swap two adjacent elements if they are in the wrong order. We have to do n-1 iterations before we get our sorted array(n is the number of elements)
 
-//  #include <iostream>
-//  using namespace std;
+ #include <iostream>
+ using namespace std;
 
-//  int main(){
+ int main(){
 
-//   int n;
-//   cin>>n;
+  int n;
+  cin>>n;
 
-//   int arr[n];
-//   for(int i=0; i<=n; i++){
-//     cin>>arr[i];
-//   }
+  int arr[n];
+  for(int i=0; i<=n; i++){
+    cin>>arr[i];
+  }
 
-//   int counter=1;
-//   while(counter<n){
-//     for(int i=0; i<n-counter;i++){
-//       if(arr[i]>arr[i+1]){
-//         int temp=arr[i];
-//         arr[i]=arr[i+1];
-//         arr[i+1]=temp;
-//       }
-//     }
-//     counter++;
-//   }
-//   for(int i=0; i<n; i++){
-//     cout<<arr[i]<<" ";
-//   }cout<<endl;
+  int counter=1;
+  while(counter<n){
+    for(int i=0; i<n-counter;i++){
+      if(arr[i]>arr[i+1]){
+        int temp=arr[i];
+        arr[i]=arr[i+1];
+        arr[i+1]=temp;
+      }
+    }
+    counter++;
+  }
+  for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+  }cout<<endl;
 
-//   return 0;
-//  }
+  return 0;
+ }
 
 //3)Insertion Sort:Insert an element from unsorted array to its correct position in sorted array
 
@@ -97,3 +97,4 @@
 
   return 0;
  }
+
